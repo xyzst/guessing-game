ed442d96f349011c1c89fd7432352945c9de6341
Darren Rambaud <xyzst@users.noreply.github.com>
2019-09-13 23:47:19 -0500

    Add error handling for parsing the user provided input

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,10 @@ fn main() {
         let mut guess = String::new();
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
-        let guess: u32 = guess.trim().parse()
-            .expect("Only numeric values allowed");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(number) => number,
+            Err(_) => continue,
+        };
         println!("You guessed: {}", guess);
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small"),
